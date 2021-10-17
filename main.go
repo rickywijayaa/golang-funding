37af6205c19e9fbf6423cb53f7d382ee73830a02
Ricky Wijaya <ricky.w@weefer.co.id>
2021-10-17 16:37:03 +0700

    Get User Data from Database

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	env "funding/env"
+	"funding/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -9,8 +11,16 @@ import (
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open(env.Dsn()), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(env.Dsn()), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
+	var users []user.User
+	db.Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+	}
+
 }
