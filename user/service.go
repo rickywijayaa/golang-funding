6ba23d0ae7a985abe69b90e23294663b86e9c5ee
Ricky Wijaya <ricky.w@weefer.co.id>
2021-11-05 14:36:10 +0700

    Login Service layer

--- user/service.go
+++ user/service.go
@@ -1,9 +1,14 @@
 package user
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Service interface {
 	RegisterUser(input RegisterInput) (User, error)
+	LoginUser(input LoginInput) (User, error)
 }
 type service struct {
 	repository Repository
@@ -32,4 +37,25 @@ func (s *service) RegisterUser(input RegisterInput) (User, error) {
 	}
 
 	return newUser, nil
+}
+
+func (s *service) LoginUser(input LoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("No User Found Under This Email")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return user, errors.New("Wrong Password")
+	}
+
+	return user, nil
 }
