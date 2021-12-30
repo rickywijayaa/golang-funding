4c8d6c1849ecb19beaa57650a3aa409563b18638
Ricky Wijaya <ricky.w@weefer.co.id>
2021-12-30 11:00:53 +0700

    Add Cors

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"funding/user"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -37,6 +38,7 @@ func main() {
 	transactionHandler := handler.NewTransactionHandler(transactionService, paymentService)
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.Static("/images", "./images")
 	api := router.Group("/api/v1")
 
